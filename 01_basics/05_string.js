6ac1937d776e7babe534b7b30a3de4ad9f2aa11a
Prerana43 <166931461+Prerana43@users.noreply.github.com>
2025-02-19 17:28:28 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,4 +5,32 @@ const repoCount = 50
 
 console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('preranans')
+const gameName = new String('prerana-at-nishad')
+
+//console.log(gameName[0]);
+//console.log(gameName._proto_);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(4));
+//console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString); 
+
+// const anotherString = gameName.slice(-7, 3);
+// console.log(anotherString);
+
+// const newStringOne = "    prerana    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://prerana.com/prerana%20nishad"
+
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
